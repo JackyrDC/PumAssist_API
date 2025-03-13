d428857371ccf2c9b898c138aa6a86cc9cb94f64
JackDc <jackyr.discua@unah.hn>
2025-03-12 22:16:52 -0600

    Adding controlers for Campus, Classess, Daily Roll and Students

---
+++ Controllers/ClassesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PumAssist_API.Controllers
+{
+    public class ClassesController : ApiController
+    {
+        private MyAppDbContext db = new MyAppDbContext();
+
+        [HttpGet]
+        [Route("api/Classes/Get")]
+        public IEnumerable<Models.Classes> Get()
+        {
+            return db.Classes.ToList();
+        }
+
+        [HttpGet]
+        [Route("api/Classes/Get/{id}")]
+        public Models.Classes GetbyId(int id)
+        {
+          return db.Classes.Find(id);
+        }
+
+        [HttpGet]
+        [Route("api/Classes/GetByAlumn/{idAlumno}")]
+        public IEnumerable<Models.Classes> GetClassesByAlumn(int idAlumno)
+        {
+            return db.Classes.Where(c => c.StudentsList.Any(s => s.IdStudent == idAlumno)).ToList();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]Models.Classes classes)
+        {
+            db.Classes.Add(classes);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(int id, [FromBody]Models.Classes classes)
+        {
+            db.Entry(classes).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            db.Classes.Remove(db.Classes.Find(id));
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/DailyRollController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace PumAssist_API.Controllers
+{
+    public class DailyRollController : ApiController
+    {
+        private MyAppDbContext db = new MyAppDbContext();
+        [Route("/daily/getdaily")]
+        [HttpGet]
+        public IEnumerable<Models.DailyRoll> Get()
+        {
+
+            return db.DailyRolls.ToList();
+        }
+
+        [HttpGet]
+        [Route("/daily/getdaily/{id}")]
+        public Models.DailyRoll Get(int id)
+        {
+            return db.DailyRolls.Find(id);
+        }
+
+        [HttpPost]
+        [Route("/daily/postdaily")]
+        public IHttpActionResult Create()
+        {
+            try
+            {
+                Models.DailyRoll dailyRoll = new Models.DailyRoll();
+                dailyRoll.creationDate = DateTime.Now;
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("/daily/postmanydaily/")]
+        public IHttpActionResult CreateMany(IEnumerable<Models.DailyRoll> collection)
+        {
+            try
+            {
+                collection.ToList().ForEach(dailyRoll =>
+                {
+                    dailyRoll.creationDate = DateTime.Now;
+                    db.DailyRolls.Add(dailyRoll);
+                });
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("/daily/putdaily/{id}")]
+        public IHttpActionResult Edit(int id)
+        {
+            try
+            {
+                Models.DailyRoll dailyRoll = db.DailyRolls.Find(id);
+                dailyRoll.creationDate = DateTime.Now;
+                db.Entry(dailyRoll).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("/daily/deletedaily/{id}")]
+        [HttpPost]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                db.DailyRolls.Remove(db.DailyRolls.Find(id));
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}

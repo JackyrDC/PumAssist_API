6833061266a356270b3b7192d18e1a1baab57b0a
JackyrDC <djackyrcalix@gmail.com>
2025-03-30 22:44:33 -0600

    fixed the Daily rolls controller dependencies bug

--- Controllers/ClassesController.cs
+++ Controllers/ClassesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using PumAssist_API.Models;
 
 namespace PumAssist_API.Controllers
 {
--- Models/DailyRoll.cs
+++ Models/DailyRoll.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class DailyRoll
+namespace PumAssist_API.Models
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int idDailyRoll { get; set; }
-    public int idRoll { get; set; }
-    [ForeignKey("idRoll")]
-    public virtual Roll Roll { get; set; }
+    [Table("DailyRoll")]
+    public class DailyRoll
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idDailyRoll { get; set; }
 
-    public System.DateTime creationDate { get; set; }
+        [ForeignKey("Roll")]
+        public int idRoll { get; set; }
 
-    public virtual ICollection<PermanentRoll> studentsList { get; set; }
+        public System.DateTime creationDate { get; set; }
 
-    public bool IsDeleted { get; set; } = false;
+        public virtual Roll roll { get; set; }
+
+        public virtual ICollection<PermanentRoll> studentsList { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
 }
